1d3af81e10bb0b4cf3bedd09bc770d7559e66607
WankerChallenger <bilelknh@gmail.com>
2018-10-08 09:31:45 +0200

    Merge branch 'master' of https://github.com/Noctalium/mB8Vp8e8
    # Conflicts:

--- tpl/footer.php
+++ tpl/footer.php
@@ -22,7 +22,7 @@
                     </div>
                     <ul class="links">
                         <li><a href="index.html">Accueil</a></li>
-                        <li><a href="about.html">À propos</a></li>
+                        <li><a href="about.html">À >Propos</a></li>
                         <li><a href="services.html">Services</a></li>
                         <li><a href="gallery.html">Galerie</a></li>
                         <li><a href="contact.html">Contact</a></li>
