f66a7be453f5988a448b3d2fefb266813c46f922
lean0 <leandre_kamanzi@yahoo.com>
2018-11-19 09:41:06 +0100

    12.03//log-repetition

---
+++ session/register.php
@@ -0,0 +1,64 @@
+<div class="modal fade" id="exampleModal1" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel1" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel1">Register</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form action="#" method="post">
+                    <div class="form-group">
+                        <label for="recipient-name" class="col-form-label">Username</label>
+                        <input type="text" class="form-control" placeholder=" " name="Name" id="recipient-rname" required="">
+                    </div>
+                    <div class="form-group">
+                        <label for="recipient-email" class="col-form-label">Email</label>
+                        <input type="email" class="form-control" placeholder=" " name="Email" id="recipient-email" required="">
+                    </div>
+                    <div class="form-group">
+                        <label for="password1" class="col-form-label">Password</label>
+                        <input type="password" class="form-control" placeholder=" " name="Password" id="password1" required="">
+                    </div>
+                    <div class="form-group">
+                        <label for="password2" class="col-form-label">Confirm Password</label>
+                        <input type="password" class="form-control" placeholder=" " name="Confirm Password" id="password2" required="">
+                    </div>
+                    <div class="sub-w3l">
+                        <div class="sub-agile">
+                            <input type="checkbox" id="brand2" value="">
+                            <label for="brand2" class="mb-3">
+                                <span></span>I Accept to the Terms & Conditions</label>
+                        </div>
+                    </div>
+
+                    <div class="right-w3l">
+                        <input type="submit" class="form-control serv_bottom" value="Register">
+                    </div>
+
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- // register -->
+
+<!-- script for password match -->
+<script>
+    window.onload = function () {
+        document.getElementById("password1").onchange = validatePassword;
+        document.getElementById("password2").onchange = validatePassword;
+    }
+
+    function validatePassword() {
+        var pass2 = document.getElementById("password2").value;
+        var pass1 = document.getElementById("password1").value;
+        if (pass1 != pass2)
+            document.getElementById("password2").setCustomValidity("Passwords Don't Match");
+        else
+            document.getElementById("password2").setCustomValidity('');
+        //empty string means no validation error
+    }
+</script>
+<!-- script for password match -->
