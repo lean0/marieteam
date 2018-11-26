e5a65aa39037b8b4710b76798e861b1d1d25d43c
Noctalium <43336999+Noctalium@users.noreply.github.com>
2018-11-26 11:16:55 +0100

    EFG

--- loginuser.php
+++ loginuser.php
@@ -1,20 +1,34 @@
 <?php
 require("tpl/header.php");
 require("global.php");
-?>
-<?php
 
 if(isset($_POST['mail']) && isset($_POST['password'])) {
 
     $mail = $_POST['mail'];
     $password = $_POST['password'];
-
-
+    
     $req = $db->connection()->prepare('SELECT mail,password FROM client WHERE mail LIKE ?');
-    $req->execute([$mail], $pww= array($_GET['password']));
-    echo $pww;
-    if($password==$req->fetch(password)){
+    $req->execute([$mail]);
+    $rows = $req->rowCount();
+    $data = $req->fetch();
+    if ($rows == 1) {
 
+        $pcheck = $data['password'];
+        echo "User exist </br>";
+        print "Mail: " .  $data['mail'] . "</br>";
+
+        if (password_verify($password, $pcheck)) {
+            echo "Je respectes les femmes";
+        }
+        else {
+            echo "Mot de passe incorrecte";
+        }
+    }
+    else {
+
+        echo "User doesn't exist";
 
     }
+
+
 }
