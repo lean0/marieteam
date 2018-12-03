da5b83e3f74b4dd6275e4e9431623e27139cdac9
Noctalium <43336999+Noctalium@users.noreply.github.com>
2018-12-03 09:06:05 +0100

    session

--- loginuser.php
+++ loginuser.php
@@ -29,6 +29,16 @@ if(isset($_POST['mail']) && isset($_POST['password'])) {
         //Alerte Bootstrap USER
 
     }
-
-
+    if ($mail == $_POST['mail'] && $password == $_POST['password']) {
+        session_start ();
+        $_SESSION['login'] = $_POST['mail'];
+        $_SESSION['pwd'] = $_POST['password'];
+        //header ('location: index.php');
+    }
+    else {
+        echo '<body onLoad="alert(\'Membre non reconnu...\')">';
+    }
+}
+else {
+    echo 'Les variables du formulaire ne sont pas déclarées.';
 }
