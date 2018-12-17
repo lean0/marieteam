9fefc6d122caa7a6af3d14844846964c69e690f1
WankerChallenger <bilelknh@gmail.com>
2018-12-17 09:54:53 +0100

    login

--- Dashboard/createbateau.php
+++ Dashboard/createbateau.php
@@ -17,7 +17,7 @@ require("../global.php");
             <div class="alert alert-success" id="success-alert">
                 <button type="button" class="close" data-dismiss="alert">x</button>
                 <strong>Success! </strong>
-                Product have added to your wishlist.
+                Ajout Bateau
             </div>
             <script>
                 $("#success-alert").fadeTo(2000, 500).slideUp(500, function(){
