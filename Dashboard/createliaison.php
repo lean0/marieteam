e666c6c35f98e883fdb6a3d91f8bab34b2e755f2
RedhaSlh <redha.souilah@gastonberger.fr>
2018-12-17 11:09:15 +0100

    Merge branch 'master' of https://github.com/Noctalium/mB8Vp8e8
    # Conflicts:
    #	Dashboard/user.php

---
+++ Dashboard/createliaison.php
@@ -0,0 +1,19 @@
+<?php
+
+require("../global.php");
+
+if(isset($_POST['selectPortDepart']) && isset($_POST['selectPortArriver']) && isset($_POST['distance'])) {
+    $SPD = $_POST['selectPortDepart'];
+    $SPA = $_POST['selectPortArriver'];
+    $distance = $_POST['distance'];
+
+    $req = $db->connection()->prepare('INSERT INTO liaison (portDepart, portArriver, distance) VALUE (?,?,?)');
+    $req->execute([$SPD, $SPA, $distance]);
+    $success=1;
+
+    header('Location: liaison.php?success=1');
+
+
+}
+
+
