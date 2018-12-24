04f37764a21a5826805895c057472af3ba7afe06
redha <redha.souilah@gastonberger.fr>
2018-12-24 14:32:48 +0100

    Auto logout system for panel and website

--- global.php
+++ global.php
@@ -2,3 +2,8 @@
 session_start ();
 require("class/Database.php");
 $db = new Database();
+$t=time();
+if (isset($_SESSION['logged']) && ($t - $_SESSION['logged'] > 1800)) {
+    session_destroy();
+    session_unset();
+}else {$_SESSION['logged'] = time();}
