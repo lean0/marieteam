558d1d44d7a8d5bee83678d0c4ebc253021f7b5d
Noctalium <noctaflygame@gmail.com>
2019-05-02 22:16:52 +0200

    [Feature] Customers page show & modify + Comment

--- session/editusersqlC.php
+++ session/editusersqlC.php
@@ -7,13 +7,13 @@ require("../global.php");
         $mail = $_POST['mailUser'];
 
 
+        //Si le champ password est set (si il fait au moins 1 char)
         if (strlen($_POST['passwordC']) > 0) {
-            echo "password change 1";
             $ps = $_POST['passwordC'];
             $psc = $_POST['passwordConfC'];
 
+            //verify si les 2 champs password et comfirm password correspondent
             if ($ps == $psc) {
-                echo "/password are ok";
                 $PH = password_hash($ps, PASSWORD_DEFAULT);
                 $req = $db->connection()->prepare('UPDATE client SET nom = :nom, prenom = :prenom, mail = :mail, password = :password WHERE idClient=' . $id);
                 $req->execute(array(
@@ -23,12 +23,14 @@ require("../global.php");
                     'password' => $PH
                 ));
                 header('Location: ../compte.php?key=' . $id);
+
+                //Si les deux champs ne correspondent pas, retourne sur compte.php avec un message d'erreur
             } else {
-                echo "password are not ok";
                 header('Location: ../compte.php?key=' . $id . '&error=1');
             }
+            //Si le champ password n'a pas été rempli, update uniquement les autres champs
+            //(Pour éviter de set le password du compte en un password blanc)
         } else {
-            echo "password do not change";
             $req = $db->connection()->prepare('UPDATE client SET nom = :nom, prenom = :prenom, mail = :mail WHERE idClient=' . $id);
             $req->execute(array(
                 'nom' => $nom,
